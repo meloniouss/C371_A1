937e5a1dd3110e51495f5b727b4fcffa6272474e
meloniouss <andrei.jianu31@gmail.com>
2025-01-31 14:00:25 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <random>
+#include <iostream>
+
+
+// Function declarations (forward declarations)
+int* createArray(int size);
+void printArray(int* array, int size);
+void deleteArray(int* array);
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Please enter an array size: ";
+    int size;
+    std::cin >> size;
+    int* array = createArray(size);
+    printArray(array, size);
+
+    deleteArray(array);
+    return 0;
+}
+
+// Function to allocate an array dynamically
+int* createArray(int size) {
+    int* dynamicArray = new int[size];
+    for(int i = 0; i < size; i++){
+        dynamicArray[i] = rand();
+    }
+    return dynamicArray;
+}
+
+// Function to print array values
+void printArray(int* array, int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+// Function to deallocate memory
+void deleteArray(int* array) {
+    delete[] array;
+}
